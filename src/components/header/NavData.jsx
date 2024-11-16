4427b5787bbdc33bf84db0b734a302fa11c185a2
philipNgungi037 <philipngungi037@gmail.com>
2024-11-16 07:10:42 +0300

    :safety_vest: add contions on my navigation data on whether user is admin or not.

--- src/components/header/NavData.jsx
+++ src/components/header/NavData.jsx
@@ -1,14 +1,14 @@
-const Navs = (username) => [
+const Navs = (username, role) => [
     {
         id: 1,
         name: username ? username : 'Account',
-        link: username ? 'profile' : 'signin',
+        link: username ? (role === 'admin' ? '/admin-dashboard' : '/profile') : '/signin',
         icon: '<ion-icon name="person-outline"></ion-icon>',
         //to be displayed by the account/user dropdown.
         subnavs: [
             { name: 'Orders', link: '/orders' },
-            { name: 'Profile', link: '/profile' },
-            { name: 'Saved Items', link: '/saved-items' }
+            { name: role === 'admin' ? 'Dashboard' : 'Profile', link: role === 'admin' ? '/admin-dashboard' : '/profile' },
+            { name: role == 'admin' ? 'Supply' : 'Saved Items', link: '/saved-items' }
         ] 
     },
     {
