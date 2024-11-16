81a89832480037e109b90d4ef2fc02e5f0d52723
philipNgungi037 <philipngungi037@gmail.com>
2024-11-16 07:16:09 +0300

    :art::lipstick: to much commits, my bad, i can't even explain the mistake i made....

--- src/components/header/NavList.jsx
+++ src/components/header/NavList.jsx
@@ -1,73 +1,73 @@
-import React, { useState, useEffect } from 'react'
-import Navs from './NavData'
+import React, { useState, useEffect } from 'react';
+import Navs from './NavData';
 import { NavLink } from 'react-router-dom';
-import {
-    useAuth
-} from '../contexts/AuthContext';
+import { useAuth } from '../contexts/AuthContext';
+
 const NavList = () => {
-    // state to open/close toggle-menu
     const [isOpen, setIsOpen] = useState(false);
     const [username, setUsername] = useState(null);
-    const [isDropdownOpen, setDropdownOpen] = useState(false); // State for dropdown whether its visible or not
+    const [role, setRole] = useState(null); // Add role state
+    const [isDropdownOpen, setDropdownOpen] = useState(false);
 
-    const { logout } = useAuth(); // Destructure the logout function from useAuth
+    const { logout } = useAuth();
 
-
-    //function to toggle
     const toggleMenu = () => {
         setIsOpen(!isOpen);
     };
 
-    // Toggle dropdown
     const toggleDropdown = () => {
         setDropdownOpen(!isDropdownOpen);
     };
 
-    // // Check user info on component mount
     useEffect(() => {
         const user = JSON.parse(localStorage.getItem('user'));
-
-        if (user && user.username) {
-            setUsername(user.username); // Set the username if user is logged in
+        if (user) {
+            setUsername(user.username);
+            setRole(user.role); // Set the user's role
         } else {
-            setUsername(null); // Clear username if not logged in
+            setUsername(null);
+            setRole(null); // Clear role if no user is logged in
         }
     }, []);
 
-
-    // Get the updated NavData based on username state
-    const navItems = Navs(username);
-
+    // Get the updated NavData based on username and role
+    const navItems = Navs(username, role);
 
     return (
-        <div className='nav-list flex justify-end items-center'>
-            <nav className='navs'>
-
-                {/* Access Navs data, then map over it displaying them in flex list, the second last item in the list is backgrounded as yellow */}
-                <ul className='flex justify-end gap-2 text-xs lg:text-sm '>
+        <div className="nav-list flex justify-end items-center">
+            <nav className="navs">
+                <ul className="flex justify-end gap-2 text-xs lg:text-sm">
                     {navItems.map((nav, index) => (
                         <li
-                            className={`nav-item ${index === navItems.length - 2 ? 'yellow-bg' : ''}`}
+                            className={`nav-item ${
+                                index === navItems.length - 2 ? 'yellow-bg' : ''
+                            }`}
                             key={index}
                         >
-                            {/* For the account/username item, handle dropdown toggle on click */}
                             {nav.subnavs ? (
-                                <a href="#!" onClick={toggleDropdown} className='cursor-pointer'>
-                                    <span dangerouslySetInnerHTML={{ __html: nav.icon }} className='mr-1' />
+                                <a
+                                    href="#!"
+                                    onClick={toggleDropdown}
+                                    className="cursor-pointer"
+                                >
+                                    <span
+                                        dangerouslySetInnerHTML={{ __html: nav.icon }}
+                                        className="mr-1"
+                                    />
                                     {nav.name}
                                 </a>
                             ) : (
                                 <NavLink to={nav.link}>
-                                    <span dangerouslySetInnerHTML={{ __html: nav.icon }} className='mr-1' />
+                                    <span
+                                        dangerouslySetInnerHTML={{ __html: nav.icon }}
+                                        className="mr-1"
+                                    />
                                     {nav.name}
                                 </NavLink>
                             )}
 
-                            {/* Conditionally render dropdown when subnavs exist and dropdown is open */}
                             {nav.subnavs && isDropdownOpen && (
-                                <ul className='dropdown bg-white z-50 mt-4 shadow-md shadow-black  rounded p-2 absolute'>
-
-                                    {/* // add signin link in the dropdown if not logged in. */}
+                                <ul className="dropdown bg-white z-50 mt-4 shadow-md shadow-black rounded p-2 absolute">
                                     {!username && (
                                         <li>
                                             <NavLink
@@ -80,15 +80,16 @@ const NavList = () => {
                                     )}
                                     {nav.subnavs.map((subnav, subIndex) => (
                                         <li key={subIndex}>
-                                            <NavLink to={subnav.link} className="block py-1 px-2 hover:bg-gray-200">
+                                            <NavLink
+                                                to={subnav.link}
+                                                className="block py-1 px-2 hover:bg-gray-200"
+                                            >
                                                 {subnav.name}
                                             </NavLink>
                                         </li>
                                     ))}
-
-                                    {/* Add Logout option */}
-                                    {username &&
-                                        (<li>
+                                    {username && (
+                                        <li>
                                             <button
                                                 onClick={logout}
                                                 className="block w-full text-left text-red-400 py-1 px-2 border border-red-400 rounded-sm hover:bg-gray-200"
@@ -103,24 +104,20 @@ const NavList = () => {
                     ))}
                 </ul>
             </nav>
-
-            {/* //menu-button */} 
-            {/* this icon i am planning to use it in future, for now i preserve it. */}
             <div className="menu hidden">
                 <button
                     onClick={toggleMenu}
-                    className="menu-button text-xl hover:border hover:border-yellow-400 "
+                    className="menu-button text-xl hover:border hover:border-yellow-400"
                 >
-                    {isOpen
-                        ? <ion-icon name="close-outline" />
-                        : <ion-icon name="menu-outline" />}
+                    {isOpen ? (
+                        <ion-icon name="close-outline" />
+                    ) : (
+                        <ion-icon name="menu-outline" />
+                    )}
                 </button>
             </div>
-
-
         </div>
+    );
+};
 
-    )
-}
-
-export default NavList
+export default NavList;
