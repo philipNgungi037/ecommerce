bda25184af6deb44a4dfb2d6834b9aa67100a5e6
philipNgungi037 <philipngungi037@gmail.com>
2024-11-27 17:24:52 +0300

    :fire: remove the mock-data variables to be replaced by api end...

--- src/components/contexts/product-context/ProductContext.jsx
+++ src/components/contexts/product-context/ProductContext.jsx
@@ -4,18 +4,7 @@ import ProductsData from '../../landing-page/products/ProductsData';
 const ProductContext = createContext();
 
 export const ProductProvider = ({ children }) => {
-    // products categorized-varables that can be accessed anywhere in the app by use of `useContext`
-    const products = ProductsData;
-    const AllProducts = products;
-
-    // use a Set named uniqueCategories to store unique category titles and iterate over each product in ProductsData, adding its category title to the uniqueCategories set.
-    // Then use Array.from() to convert the set to an array and then use map() to create an array of category objects with only the title and img properties.
-    const uniqueCategories = new Set();
-    ProductsData.forEach(product => uniqueCategories.add(product.category.title));
-    const AllCategories = Array.from(uniqueCategories).map(categoryTitle => {
-        const product = ProductsData.find(product => product.category.title === categoryTitle);
-        return product.category;
-    });
+   
 
 
     const TopOffers = products.filter(product => product.discount > 20);
