4cec078fae6267f5432355bbdd6538ce1c204d06
philipNgungi037 <philipngungi037@gmail.com>
2024-11-30 11:53:18 +0300

    :art: create an array of items to be listed in our sidebar.

--- src/components/admin/AdminData.jsx
+++ src/components/admin/AdminData.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
 
-const AdminData = () => {
-  return (
-    <div>AdminData</div>
-  )
-}
+const AdminData = () => [
+    {
+        id: '1',
+        name: 'products',
+        link: 'products'
+    },
+    {
+        id: '2',
+        name: 'customers',
+        link: 'users'
+    },
+    {
+        id: '3',
+        name: 'orders',
+        link: 'orders'
+    },
+    {
+        id: '4',
+        name: 'sales',
+        link: 'sales'
+    },
+    {
+        id: '5',
+        name: 'settings',
+        link: 'settings'
+    }
+    
+]
 
 export default AdminData
