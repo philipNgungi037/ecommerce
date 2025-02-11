d5d2466f91e1154e402963a143a1434cca8c5b83
philipNgungi037 <philipngungi037@gmail.com>
2025-02-11 13:47:43 +0300

    :sparkles::art::package: create and render  admin-dashboard component...

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Route, Routes } from "react-router";
 import Signin from './pages/authentication-pages/Signin'
 import Signup from './pages/authentication-pages/Signup';
 import { AuthProvider } from './components/contexts/AuthContext';
+import AdminDashboard from './components/admin/admin-dashboard/AdminDashboard';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
             <Route path='/' element={<LandingPage />} />
             <Route path='signin' element={<Signin />} />
             <Route path='register' element={<Signup />} />
+            <Route path='admin-dashboard' element= {<AdminDashboard/>}/>
 
           </Routes>
           {/* <LandingPage /> */}
